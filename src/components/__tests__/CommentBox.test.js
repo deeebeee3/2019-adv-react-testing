44ea71a75e72ee22ad5eb307a1372d303c1abec5
Deepak Bhari <deepakbhari@Deepaks-MacBook-Pro.local>
2019-10-15 15:33:03 +0100

    AfterEach Statements

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
-it('has a text area and a buttton', () => {
+let wrapped;
 
+beforeEach(() => {
   //enzyme will take our component and render it into the fake DOM that is created by JSDOM
   //and will return the object we refer to as wrapped
-  const wrapped = mount(<CommentBox />);
+  wrapped = mount(<CommentBox />);
+});
 
+afterEach(() => {
+  //remove component from fake DOM
+  wrapped.unmount();
+})
+
+it('has a text area and a buttton', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
-
 });
