dfc0d588115acfbc8307ecff81e4ba939e2c4714
Deepak Bhari <deepakbhari@Deepaks-MacBook-Pro.local>
2019-10-16 13:08:04 +0100

    Getting data into Redux

---
+++ src/components/__tests__/CommentList.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme'; //full DOM rendering
+import CommentList from 'components/CommentList';
+import Root from 'Root';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <CommentList />
+    </Root>
+  )
+});
+
+it('creates one LI per comment', () => {
+
+});
